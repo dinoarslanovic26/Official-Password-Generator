47672315f0ac179dbaf777d04813cd8a42211daa
Dino Arslanovic <dino99arslanovic@hotmail.com>
2022-12-11 20:27:22 -0600

    Started to have prompts being asked that involve the criteria of characters and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,13 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
-
+function questions() {
+  var isValid = false;
+  do{
+    var length = prompt("Choose password length that is between 8 and 128 characters");
+    var askNumbers = confirm("Do you want the password to include numbers?");
+    
+  }
 
 
 
