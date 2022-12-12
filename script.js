8d87920f2ff719e5972974e83a63081e220887b3
Dino Arslanovic <dino99arslanovic@hotmail.com>
2022-12-12 02:04:55 -0600

    Added a funtion where options are made to the user in order to meet minimum requirements and started on another funtion where it takes all the user input to create the password

--- script.js
+++ script.js
@@ -11,10 +11,41 @@ function questions() {
   do{
     var length = prompt("Choose password length that is between 8 and 128 characters");
     var askNumbers = confirm("Do you want the password to include numbers?");
-    
+    var askUpperCase = confirm("Do you want the password to include uppercase letters?")
+    var askLowerCase = confirm("Do you want the password to include lowercase numbers?")
+    var askSpecial = confirm("Do you want the password to include special characters?")
+    var responses = {
+      length: length,
+      askNumbers: askNumbers,
+      askUpperCase: askUpperCase,
+      askLowerCase: askLowerCase,
+      askSpecial: askSpecial  
+    }
+    if((length < 8)||(length > 128))
+    alert("Chhoose a number between 8 and 128");
+    else if((!askNumbers)&&(!askUowerCase)&&(!askLowerCase)&&(!askSpecial))
+    alert("You Must choose at least one type for your password.")
+    else
+    isValid = true;
+
+  }while(!isValid)
+  return responses;
   }
 
+function generatePassword(){
+  var passwordOptions = questions();
+  var possibleCombo = [];
+  var finalPassword = "";
 
+  if (passwordOptions.askNumbers){
+    for (var i of numbers)
+    possibleCombo.push(i);
+  }
+}
+  if (passwordOptions.askUowerCase){
+    for (var i of upperCase)
+    possibleCombo.push(i);
+  }
 
 
 // Write password to the #password input
